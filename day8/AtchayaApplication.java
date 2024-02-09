29743e2e4b1b0abb8a3b0660056bb02930e69676
AtchayaSubramanian <118709320+AtchayaSubramanian@users.noreply.github.com>
2024-02-09 15:35:54 +0530

    Add files via upload

---
+++ day8/AtchayaApplication.java
@@ -0,0 +1,13 @@
+package com.party.atchaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtchayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtchayaApplication.class, args);
+	}
+
+}
---
+++ day8/Enum/Role.java
@@ -0,0 +1,5 @@
+package com.party.atchaya.Enum;
+
+public enum Role {
+ADMIN,USER
+}
